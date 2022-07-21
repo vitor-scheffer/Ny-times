3cdc6b18142f78b8395a04f234247de37356340b
Vitor Scheffer <vitorscheffer61@gmail.com>
2022-07-21 17:17:06 -0300

    Arquivo base.

---
+++ src/pages/Header/Header.js
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li></li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+export default Header
---
+++ src/pages/Header/Nav.js
@@ -0,0 +1,16 @@
+import {Link} from 'react-router-dom'
+
+const Nav = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link></Link>
+          <Link></Link>
+          <Link></Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+export default Nav
