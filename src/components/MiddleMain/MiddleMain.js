15940ee3af934d877d713c40502c6f231722dadf
Vitor Scheffer <vitorscheffer61@gmail.com>
2025-01-10 17:52:49 -0300

    fix multimedia error

--- src/components/MiddleMain/MiddleMain.js
+++ src/components/MiddleMain/MiddleMain.js
@@ -1,32 +1,29 @@
-import News from '../FirstNews/News'
-import styles from './MiddleMain.module.css'
+import News from "../FirstNews/News";
+import styles from "./MiddleMain.module.css";
 
-const MiddleMain = ({middleNews}) => {
-  const middleExists = Object.keys(middleNews).length > 0;
+const MiddleMain = ({ middleNews }) => {
+  const middleExists = middleNews.length > 0;
 
   return (
     <>
-    {middleExists ? (
-      <>
-      <section className={styles.flexContainer}>
-      {/* <nav className={styles.navNews}>
-        <a href="#"><strong>Russia-Ukraine War</strong></a>
-        <ul className={styles.listaNews}>
-          <li><a href="#">Maps: Russia's Invasion</a></li>
-          <li><a href="#">Photos</a></li>
-          <li><a href="#">Ukraine’s War Dead</a></li>
-          <li><a href="#">Attacks on Civilians</a></li>
-        </ul>
-      </nav> */}
-        <div>
-          <News isBorder mainNews={middleNews[0]} descricao={middleNews[0].abstract}/>
-          <News mainNews={middleNews[1]}/>
-        </div>
-        <img src={middleNews[0].multimedia[1].url} alt="" />  
-      </section>
-      </>
-    ) : (<div></div>) }
+      {middleExists ? (
+        <>
+          <section className={styles.flexContainer}>
+            <div>
+              <News
+                isBorder
+                mainNews={middleNews[0]}
+                descricao={middleNews[0].abstract}
+              />
+              <News mainNews={middleNews[1]} />
+            </div>
+            <img src={middleNews[0]?.multimedia[1]?.url} alt="" />
+          </section>
+        </>
+      ) : (
+        <div></div>
+      )}
     </>
-  )
-}
-export default MiddleMain
+  );
+};
+export default MiddleMain;
