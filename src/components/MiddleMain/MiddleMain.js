77019632ee7bd9025b7e510f51d627a9659293f0
Vitor Scheffer <vitorscheffer61@gmail.com>
2025-01-10 17:59:11 -0300

    Update MiddleMain.js

--- src/components/MiddleMain/MiddleMain.js
+++ src/components/MiddleMain/MiddleMain.js
@@ -3,6 +3,7 @@ import styles from "./MiddleMain.module.css";
 
 const MiddleMain = ({ middleNews }) => {
   const middleExists = middleNews.length > 0;
+  const imageUrl = middleNews[0]?.multimedia?.find((media) => media.url)?.url;
 
   return (
     <>
@@ -17,7 +18,13 @@ const MiddleMain = ({ middleNews }) => {
               />
               <News mainNews={middleNews[1]} />
             </div>
-            <img src={middleNews[0]?.multimedia[1]?.url} alt="" />
+            <img
+              src={
+                imageUrl ||
+                "https://ropercenter.cornell.edu/sites/default/files/styles/800x600/public/Images/New-York-Times-Logo8x6_0.png?itok=7YqGOSMA"
+              }
+              alt="News Image"
+            />
           </section>
         </>
       ) : (
